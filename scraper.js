9228098a18b5330da0d9238f14796b03ff71881f
Noah Conley <gallardo1226@gmail.com>
2014-03-03 20:05:20 -0600

    all relevant data scraped, next step: storage

--- scraper.js
+++ scraper.js
@@ -19,20 +19,39 @@ $(document).ready(function() {
 							cache: false,
 							success: function(data) {
 								var scrape = $("<div>").html(data)[0].getElementsByTagName('div');
+								// console.log(scrape);
 								var infoList = scrape[scrape.length - 6].getElementsByTagName('p');
-								var addressInfo = infoList[0].childNodes;
-								// console.log(infoList);
-								// console.log(infoList.length);
-								var infoCard = {
-									'advantage' : scrape[scrape.length - 4].textContent,
-									'address' : addressInfo[2].textContent + '\n' + addressInfo[4].textContent,
-									'mapLink' : addressInfo[6].href,
-									'phone' : infoList[1].textContent,
-									'website' : infoList[2].textContent,
-									'email' : infoList[3].textContent,
-									// 'users' : infoList[infoList.length - 2].textContent
+								var users = scrape[scrape.length - 5].lastChild.textContent;
+								var infoCard = new Object();
+								infoCard.advantage = scrape[scrape.length - 4].textContent;
+								// console.log(users);
+								// console.log(users.length);
+								// console.log(users.indexOf(': Yes'));
+								// console.log(users.lastIndexOf(': Yes'));
+								for (var k = 0; k < infoList.length; k++) {
+									var current = infoList[k].textContent;
+									if (current.indexOf('Address:') > -1) {
+										var addressInfo = infoList[k].childNodes;
+										infoCard.address = addressInfo[2].textContent + '\n' + addressInfo[4].textContent;
+										infoCard.mapLink = addressInfo[6].href;
+									}
+									else if (current.indexOf('Phone:') > -1)
+										infoCard.phone = infoList[k].textContent;
+									else if (current.indexOf('Web Site:') > -1)
+										infoCard.website = infoList[k].textContent;
+									else if (current.indexOf('Email:') > -1)
+										infoCard.email = infoList[k].textContent;
 								}
+								// console.log(infoCard);
 								// console.log(addressInfo[6].attributes);
+								if (users.indexOf(': Yes') == 18) {
+									if (users.lastIndexOf(': Yes') == 161)
+										infoCard.userGroup = 'All Cardholders';
+									else
+										infoCard.userGroup = 'Students Only';
+								} else
+									infoCard.userGroup = 'Faculty Only';
+								// console.log(infoCard.userGroup);
 								business.info = infoCard;
 								// console.log(business);
 							}
