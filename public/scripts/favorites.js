489441249473d107204658810f68ad8f3f3188b2
Priscilla Liu <priscilla.i.liu@gmail.com>
2014-03-16 09:20:55 -0500

    functionality with edit button -> delete icon -> delete box troubles still with delete icon toggle need to code delete box needs styling

--- public/scripts/favorites.js
+++ public/scripts/favorites.js
@@ -2,20 +2,25 @@
 $(document).ready(function() {
 	for (i = 0; i <= 240; i++) {
 		if (localStorage.getItem('business_' + i) != null) {
-			$('#favlist').append('<li class="list-group-item">\n<img class="delete hidden" src="public/images/delete_button.png" alt="Delete"><a href="'+ localStorage.getItem('business_' + i + '_url') + '">\n<div class="row black">\n<div class="col-xs-8">\n<h4>'+ localStorage.getItem('business_' + i) + '</h4>\n</div>\n<div class="col-xs-4 desc">\n<h6>'+ localStorage.getItem('business_' + i + '_adv') + '</h6>\n</div>\n</div>\n</a>\n<div class="DeleteBox hidden">Delete</div></li>\n');
+			$('#favlist').append('<li class="list-group-item">\n<img id="dButton'+i+'" class="delete" src="public/images/delete_button.png" alt="Delete"><a href="'+ localStorage.getItem('business_' + i + '_url') + '">\n<div class="row black">\n<div class="col-xs-8">\n<h4>'+ localStorage.getItem('business_' + i) + '</h4>\n</div>\n<div class="col-xs-4 desc">\n<h6>'+ localStorage.getItem('business_' + i + '_adv') + '</h6>\n</div>\n</div>\n</a>\n<div id="dBoxdButton'+i+'" class="DeleteBox">Delete</div></li>\n');
 		}
 	}
+
+
 	if ($('li').length == 0)
 		$('#favlist').after('<h4 style="text-align:center">Tap \"Add to Favorites\" on any business info screen to see it appear here');
 
+
+	$(".delete").hide();
+	$(".DeleteBox").hide();
+
 	$('#edit').click(function(){
-		//$(".delete").toggle(); //this isn't working for some reason...
-		$(".delete").removeClass("hidden");
+		$(".delete").toggle();
 	})
 
 	$(".delete").click(function(){
-		$(".delete").addClass("hidden");
-		$(".DeleteBox").removeClass("hidden");
+		$(this.id).toggle(); //not working
+		$("#dBox"+this.id).toggle();
 	})
 
 	$(".DeleteBox").click(function(){
