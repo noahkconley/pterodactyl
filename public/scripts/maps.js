e3a673685772a0f32b12e4a587c1e0265ea479a9
Grygoree <GregoryLeung2016@u.northwestern.edu>
2014-03-17 05:08:08 -0500

    Clickable Markers for demo
    edited maps.js to include markers

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,29 +1,76 @@
 var latLngBounds = new google.maps.LatLngBounds();
 var hardinHall = new google.maps.LatLng(42.050625, -87.679664);
 var geocoder = new google.maps.Geocoder();
-var i = 0;
+var i = 0;  //counter for the amount of markers for demo to be requested by geolocation
+var markerCount = 0;  //counter for markers that successfully get set
+
+//store addresses for demo
 var addressArray = [
 "527 Davis Street%20Evanston, IL 60201",  
-"800 Dempster St.%20Evanston, IL 60202",  
-"3500 W. Dempster St.%20Evanston, IL 60201",  
+"800 Dempster St.%20Evanston, IL 60202",    
 "1724 Sherman Ave.%20Evanston, IL 60201",  
 "928 Chicago Ave.%20Evanston, IL 60202",  
-"1609 Sherman Avenue, Suite 208%20Evanston, IL 60201", 
+"1609 Sherman Avenue, Suite 208%20Evanston, IL 60201",
+
 "915 Elmwood Ave.%20Evanston, IL 60202",  
 "1322 Chicago Ave.%20Evanston, IL 60201",  
 "907 1/2 Sherman Ave.%20Evanston, IL 60201",  
 "924 Davis St.%20Evanston, IL 60201",  
-"518 Davis St.%20Evanston, IL 60201",  
-"2114 Central St.%20Evanston, IL 60201",  
+"518 Davis St.%20Evanston, IL 60201",    
+
 "1712 Sherman Ave. Alley%20Evanston, IL 60201",  
 "1640 Orrington Ave%20Evanston, IL 60201",  
 "737 Chicago Ave.%20Evanston, IL 60202",  
-"719 Church Street%20Evanston, IL 60201",  
-"826 Noyes Street%20Evanston, IL 60201", 
-"160 E .Huron St.%20Chicago, IL, IL 60611",  
-"1511 Sherman Ave.%20Evanston, IL 60201",  
-"1212 Sherman Ave.%20Evanston, IL 60202", ]
+"719 Church Street%20Evanston, IL 60201",   
+"1511 Sherman Ave.%20Evanston, IL 60201", 
+]
+
+var infoWindowContentsArray = [ //match the address arrays
+"527 Cafe",
+"2nd Hand Tunes",
+"AAA Evanston",
+"A And A Moley Vacuum Cleaning",
+"Cold Stone Creamery",
+
+"Acupuncture Access",
+"Addis Abeba Restaurant",
+"Revolution Spin",
+"Affordable Portables",
+"Todoroki Hibachi and Sushi",
+
+"Alley Gallery",
+"American Defensive Driving School",
+"AMLI Evanston",  
+"Flat Top Grill",
+"Aylas Originals",
+]
+
+  var testString = 'test content';
+  /*
+   '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+  */
+
 var markerArray = new Array();// holds markers for all 20 addresses
+var infoWindowArray = new Array();
 
 function initialize() {
 
@@ -61,21 +108,19 @@ function initialize() {
     document.getElementById("map-canvas").style.height=H;
 
     if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geoPos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var geoPos = new google.maps.LatLng(position.coords.latitude,
+                                         position.coords.longitude);
 
-      latLngBounds.extend(geoPos);
+        latLngBounds.extend(geoPos);
 
-      var geoInfowindow = new google.maps.InfoWindow({
-        map: map,
-        position: geoPos,
-        content: 'You are here.'
-      });
-      map.setCenter(geoPos);
-
-      //map.setCenter(geoPos);
-        }, function() {
+        var geoInfowindow = new google.maps.InfoWindow({
+          map: map,
+          position: geoPos,
+          content: 'You are here.'
+        });
+        map.setCenter(geoPos);
+      }, function() {
            handleNoGeolocation(true);
           });
 
@@ -84,48 +129,32 @@ function initialize() {
         handleNoGeolocation(false);
       }
 
+      /*
     if(document.cookie != ""){
       address = document.cookie;
       //console.log('The address is ' + document.cookie);
       //address stored in cookie
       codeAddress(document.cookie);
     }
+    */
 
 
-    //begin drawing markers on map
-    /*
-    for(var i = 0; i<addressArray.length; i++){
-      codeAddress(addressArray[i]); //encode URI maybe?
-    }
-    */
-    /*
-    delayLoop();
-    */
+
     
-    intervalTimer = setInterval(function(){markerTimer()},500);
+    intervalTimer = setInterval(function(){markerTimer()},400);
+}
 
-    //map.fitBounds(latLngBounds);
-}
-/*
-function delayLoop() {
-  setTimeout(function(){
-    codeAddress(addressArray[i]); //encodeuri maybe?
-    i++;
-    console.log(addressArray.length);
-    console.log(addressArray[i]);
-    if( i < addressArray.length) delayLoop;
-  }, 300)
-}
-*/
 function markerTimer(){
+  console.log(addressArray[i]);
+  //add marker
+  codeAddress(addressArray[i]);
+  //add window to that marker
+  //addWindow();
+  i++;
   if( i >= addressArray.length){
     clearInterval(intervalTimer);
-    return;
+    console.log('timer called');
   }
-  console.log(addressArray[i]);
-  console.log(i);
-  codeAddress(addressArray[i]);
-  i++;
 }
 
 function handleNoGeolocation(errorFlag) {
@@ -157,14 +186,54 @@ function codeAddress(address) {
           position: results[0].geometry.location
       });
       //add the geomarker to the markerArray
-      markerArray.push(geoMarker);
+      markerArray[markerCount] = new google.maps.Marker(geoMarker);
+
+      //create an info window for this marker along with a click event
+      addWindow();
+
+      markerCount++;
+
       // add lat, lng to the bounds
       latLngBounds.extend(geoMarker.position);
     } else {
+      var geoMarker = null;
+      markerArray[markerCount] = new google.maps.Marker(geoMarker);
       console.log('Geocode was not successful for the following reason: ' + status);
     }
   });
 }
+
+function addWindow() {
+  //give each marker a window
+  console.log('infowindows called' +markerCount);
+  //add click events for each window
+    console.log(markerArray[markerCount]!=null);
+    if (markerArray[markerCount] != null){
+      console.log("the marker arrya count is " + markerCount);
+      var tempWindow = new google.maps.InfoWindow({
+        content: infoWindowContentsArray[markerCount]
+      });
+
+      infoWindowArray[markerCount] = new google.maps.InfoWindow(tempWindow);
+      //infoWindowArray[markerCount].open(map,markerArray[markerCount]);
+      
+      google.maps.event.addListener(markerArray[markerCount], 'click', callback(markerCount,map));
+
+
+    }
+    else {
+      console.log("Marker null.  Can't add window at" + i);
+    }
+}
+
+function callback(localMarkerCount,localMap){
+    //callback function, force markerCount to be evaluated for listener function
+    //instead of reference
+    return function(){
+      infoWindowArray[localMarkerCount].open(localMap,markerArray[localMarkerCount]);
+    };
+};
+
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
